bcde8f074a6fc3fe216c17129d91c8502ee656dd
Sumit Joshi <sumitjoshiakit@gmail.com>
2026-09-08 23:40:36 +0530

    Add centralized input sanitization

---
+++ js/sanitize.js
@@ -0,0 +1,15 @@
+function sanitizeText(value) {
+  if (typeof value !== "string") return "";
+  return value.replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/g, "").trim();
+}
+
+function escapeHtml(value) {
+  return sanitizeText(value)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
+export { sanitizeText, escapeHtml };
